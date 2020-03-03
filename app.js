f4c65be3573a4d666c92764d406181bf7e4479a9
Ostrowski, Maciej <maciej.ostrowski@atos.net>
2020-03-03 21:46:01 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const port = process.env.PORT || 3000
+
+const generateTitle = (title) => {
+    return `New Title ${title}`
+}
+
+const app = express()
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+    const title = generateTitle('localhost')
+
+    res.render('index', {
+        pageTitle: 'Node JS',
+        pageBody: title
+    })
+})
+
+app.get('/kontakt', (req, res) => {
+    res.send('dane kontaktowe!!')
+})
+
+app.listen(port)
